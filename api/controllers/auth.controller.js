1a11e6be7eacf19740f345d2ad10c6fb33aa4db1
CliveFion <atleclivechurchill@fionsystems.com>
2024-11-01 12:36:27 +0900

    signIn route complete

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import { application } from 'express';
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req,res, next) => {
     const {username, email, password} = req.body;
@@ -12,5 +13,22 @@ export const signup = async (req,res, next) => {
         res.status(201).json("user created successfully!");
     } catch (error) {
         next(errorHandler(550, 'error from the message'));
+    }
+}
+
+export const signin = async(req, res, next) => {
+    const {email, password} = req.body;
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404, 'User not found!'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+        res
+            .cookie('access_token', token, {httpOnly: true})
+            .status(200)
+            .json(validUser);
+    } catch (error) {
+        next(error);
     }
 }
